1fdf3e792cbce0a9d0cda543512dec2811ac9f07
Vikash Kumar <vikash@deepen.ai>
2021-12-27 14:19:19 +0530

    checkpoint

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,69 @@
-FROM ubuntu:xenial
+# FROM ubuntu:18.04
 
-RUN echo "deb http://de.archive.ubuntu.com/ubuntu precise main restricted universe" >> /etc/apt/sources.list
-RUN echo "deb-src http://de.archive.ubuntu.com/ubuntu precise restricted main multiverse universe" >> /etc/apt/sources.list
-RUN echo "" >> /etc/apt/sources.list
-RUN echo "deb http://de.archive.ubuntu.com/ubuntu precise-updates main restricted universe  " >> /etc/apt/sources.list
-RUN echo "deb-src http://de.archive.ubuntu.com/ubuntu precise-updates restricted main multiverse universe" >> /etc/apt/sources.list
+# # RUN echo "deb http://de.archive.ubuntu.com/ubuntu precise main restricted universe" >> /etc/apt/sources.list
+# # RUN echo "deb-src http://de.archive.ubuntu.com/ubuntu precise restricted main multiverse universe" >> /etc/apt/sources.list
+# # RUN echo "" >> /etc/apt/sources.list
+# # RUN echo "deb http://de.archive.ubuntu.com/ubuntu precise-updates main restricted universe  " >> /etc/apt/sources.list
+# # RUN echo "deb-src http://de.archive.ubuntu.com/ubuntu precise-updates restricted main multiverse universe" >> /etc/apt/sources.list
 
-# RUN apt-get update
-# RUN apt-get install -y libopencv-core-dev libopencv-highgui-dev libopencv-imgcodecs-dev libopencv-imgproc-dev
+# RUN apt-get update && apt-get -y upgrade
+# RUN apt install -y software-properties-common apt-file
+# RUN apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev 
+# RUN apt-get install -y libtbb2 libtbb-dev
+# RUN apt-get install -y build-essential cmake
+# RUN apt-get install -y libopencv-core-dev libopencv-highgui-dev libopencv-imgcodecs-dev libopencv-imgproc-dev libjsoncpp-dev
+# RUN apt-get install -y libjpeg-dev libpng-dev libtiff5-dev libjasper-dev libdc1394-22-dev libeigen3-
+# # RUN apt-add-repository ppa:deadsnakes/ppa
 
-WORKDIR /code
-COPY . /code/
+# # RUN apt-get update
+# WORKDIR /code
+# COPY . /code/
+# # RUN rm -rf build/ bin/ lib/ && mkdir build
+# # WORKDIR /code/build
+# # RUN cmake -DCMAKE_C_COMPILER="gcc" -DCMAKE_CXX_COMPILER="g++" .. && make
+# # WORKDIR /code/
+# # RUN ls
+# CMD ["ls"]
 
-CMD ["ls"]
+FROM veerabadralokesh/ubuntu2004-googlecloud-python3-opencv:2
+
+ARG OPENCV_VERSION="3.0.0"
+
+# install dependencies
+RUN apt-get update
+RUN apt-get install -y libopencv-dev yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils pkg-config
+RUN apt-get install -y curl build-essential checkinstall cmake
+
+# download opencv
+RUN curl -sL https://github.com/Itseez/opencv/archive/$OPENCV_VERSION.tar.gz | tar xvz -C /tmp
+RUN mkdir -p /tmp/opencv-$OPENCV_VERSION/build
+
+WORKDIR /tmp/opencv-$OPENCV_VERSION/build
+
+# install
+RUN cmake -DWITH_FFMPEG=OFF -DWITH_OPENEXR=OFF -DBUILD_TIFF=OFF -DWITH_CUDA=OFF -DWITH_NVCUVID=OFF -DBUILD_PNG=OFF ..
+RUN make
+RUN make install
+
+# configure
+RUN echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf
+RUN ldconfig
+RUN ln /dev/null /dev/raw1394 # hide warning - http://stackoverflow.com/questions/12689304/ctypes-error-libdc1394-error-failed-to-initialize-libdc1394
+
+# cleanup package manager
+# RUN apt-get remove --purge -y curl build-essential checkinstall cmake
+# RUN apt-get autoclean && apt-get clean
+# RUN rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# prepare dir
+RUN apt-get install -y libopencv-core-dev libopencv-highgui-dev libopencv-imgcodecs-dev libopencv-imgproc-dev libjsoncpp-dev
+
+RUN mkdir /source
+
+# VOLUME ["/source"]
+WORKDIR /source
+COPY . /source/
+RUN rm -rf build && rm -rf bin && rm -rf lib && mkdir build
+WORKDIR /source/build
+RUN cmake -DCMAKE_C_COMPILER="gcc" -DCMAKE_CXX_COMPILER="g++" ../ && make
+CMD ["bash"]
--- src/chessboard_corner_detect.cc
+++ src/chessboard_corner_detect.cc
@@ -12,9 +12,23 @@
 
 using namespace std::chrono;
 
-void save_board_as_json(std::vector<cbdetect::Board> &boards, cbdetect::Corner &corners, const char* outputFileName);
+struct ImageDetectionConfig {
+  std::vector<std::string> imageFilePaths;
+  std::string outputFilePath;
+};
 
-void detect(const char* str, cbdetect::CornerType corner_type, const char* outputFilePath) {
+struct ImageCornerDetectionResult {
+  std::string imageFilePath;
+  cbdetect::Corner corners;
+  std::vector<cbdetect::Board> boards;
+};
+
+void save_board_as_json(std::vector<cbdetect::Board> &boards, cbdetect::Corner &corners, const char* outputFileName);
+void readJsonFile(const char* jsonFilePath, Json::Value &jsonData);
+void get_detection_result_as_json(ImageCornerDetectionResult &imageResult, Json::Value &root);
+void format_detection_result_as_json(std::vector<cbdetect::Board> &boards, cbdetect::Corner &corners, Json::Value &root);
+
+ImageCornerDetectionResult detect(const char* str, cbdetect::CornerType corner_type, const char* outputFilePath) {
   cbdetect::Corner corners;
   std::vector<cbdetect::Board> boards;
   cbdetect::Params params;
@@ -25,20 +39,29 @@ void detect(const char* str, cbdetect::CornerType corner_type, const char* outpu
   // auto t1 = high_resolution_clock::now();
   cbdetect::find_corners(img, corners, params);
   // auto t2 = high_resolution_clock::now();
-  // cbdetect::plot_corners(img, corners);
+  cbdetect::plot_corners(img, corners);
   // auto t3 = high_resolution_clock::now();
   cbdetect::boards_from_corners(img, corners, boards, params);
   // auto t4 = high_resolution_clock::now();
   // printf("Find corners took: %.3f ms\n", duration_cast<microseconds>(t2 - t1).count() / 1000.0);
   // printf("Find boards took: %.3f ms\n", duration_cast<microseconds>(t4 - t3).count() / 1000.0);
   // printf("Total took: %.3f ms\n", duration_cast<microseconds>(t2 - t1).count() / 1000.0 + duration_cast<microseconds>(t4 - t3).count() / 1000.0);
-  // cbdetect::plot_boards(img, corners, boards, params);
-  save_board_as_json(boards, corners, outputFilePath);
+  cbdetect::plot_boards(img, corners, boards, params);
+  // save_board_as_json(boards, corners, outputFilePath);
+  return { str, corners, boards };
 }
 
-void save_board_as_json(std::vector<cbdetect::Board> &boards, cbdetect::Corner &corners, const char* outputFileName) {
-  Json::Value root;
+void get_detection_result_as_json(ImageCornerDetectionResult &imageResult, Json::Value &root) {
+  root["file_name"] = imageResult.imageFilePath;
+  format_detection_result_as_json(imageResult.boards, imageResult.corners, root);
+}
+
+void format_detection_result_as_json(std::vector<cbdetect::Board> &boards, cbdetect::Corner &corners, Json::Value &root) {
   Json::Value _boards(Json::arrayValue);
+  std::sort(boards.begin(), boards.end(),
+    [](const cbdetect::Board& b1, const cbdetect::Board& b2) {
+      return b1.num > b2.num;
+    });
   for (cbdetect::Board &board: boards) {
     Json::Value _board(Json::arrayValue);
     for (std::vector<int> &indexes: board.idx) {
@@ -59,6 +82,32 @@ void save_board_as_json(std::vector<cbdetect::Board> &boards, cbdetect::Corner &
   }
   root["board_corner_indexes"] = _boards;
   root["detected_corner_points"] = _points;
+}
+
+void create_image_detection_result_json(std::vector<ImageCornerDetectionResult> &results, Json::Value &root) {
+  for (auto& imageCornerDetectionResult: results) {
+    Json::Value _result;
+    get_detection_result_as_json(imageCornerDetectionResult, _result);
+    root.append(_result);
+  }
+}
+
+void save_image_detection_result_json(Json::Value &root, const char* outputFileName) {
+  Json::FastWriter fastWriter;
+  std::string jsonString = fastWriter.write(root);
+  std::ofstream outdata;
+  outdata.open(outputFileName);
+  if (!outdata) {
+    std::cout << "Output file opening failed!" << std::endl;
+    exit(1);
+  }
+  outdata << jsonString;
+  outdata.close();
+}
+
+void save_board_as_json(std::vector<cbdetect::Board> &boards, cbdetect::Corner &corners, const char* outputFileName) {
+  Json::Value root;
+  format_detection_result_as_json(boards, corners, root);
   Json::FastWriter fastWriter;
   std::string jsonString = fastWriter.write(root);
   // std::cout << jsonString << std::endl;
@@ -72,16 +121,46 @@ void save_board_as_json(std::vector<cbdetect::Board> &boards, cbdetect::Corner &
   outdata.close();
 }
 
+void readJsonFile(const char* jsonFilePath, Json::Value &jsonData) {
+  std::ifstream file(jsonFilePath);
+  file >> jsonData;
+}
+
+void run_process(const char* configFilePath, const char* outputFilePath) {
+  Json::Value filesConfig;
+  readJsonFile(configFilePath, filesConfig);
+  Json::Value filesList = filesConfig["files"];
+  Json::Value output(Json::arrayValue);
+  std::vector<ImageCornerDetectionResult> cornersResults;
+  for (Json::Value filePath: filesList) {
+    ImageCornerDetectionResult res = detect(filePath.asCString(), cbdetect::SaddlePoint, "random_file");
+    cornersResults.emplace_back(res);
+  }
+  create_image_detection_result_json(cornersResults, output);
+  save_image_detection_result_json(output, outputFilePath);
+}
+
 int main(int argc, char* argv[]) {
-  // printf("chessboards...");
   if (argc < 3) {
     std::cout << "Invalid Usage" << std::endl;
-    return 1;
+    exit(1);
   }
-  char* imageFilePath = argv[1];
+  char* configFilePath = argv[1];
   char* outputFilePath = argv[2];
-  detect(imageFilePath, cbdetect::SaddlePoint, outputFilePath);
-  // printf("deltilles...");
-  // detect("../../example_data/e6.png", cbdetect::MonkeySaddlePoint);
+  run_process(configFilePath, outputFilePath);
   return 0;
 }
+
+// int main(int argc, char* argv[]) {
+//   // printf("chessboards...");
+//   if (argc < 3) {
+//     std::cout << "Invalid Usage" << std::endl;
+//     return 1;
+//   }
+//   char* imageFilePath = argv[1];
+//   char* outputFilePath = argv[2];
+//   detect(imageFilePath, cbdetect::SaddlePoint, outputFilePath);
+//   // printf("deltilles...");
+//   // detect("../../example_data/e6.png", cbdetect::MonkeySaddlePoint);
+//   return 0;
+// }
